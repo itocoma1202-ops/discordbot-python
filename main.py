02d19379c510409dc158a926d29af3e8a9fb9b5d
itocoma1202-ops <itocoma1202@gmail.com>
2025-12-25 23:27:48 +0900

    改善に改善

--- main.py
+++ main.py
@@ -13,7 +13,17 @@ load_dotenv()
 
 #Render用のflaskアプリの設定
 
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Render!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    
 #intents関連
 intents=discord.Intents.none()
 intents.reactions = True
